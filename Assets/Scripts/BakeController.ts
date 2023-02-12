396d9657e0a59a47d732edd11fb78f8d378e8d08
June <cba0898@gmail.com>
2023-02-13 02:52:14 +0900

    Modify Grill script: slider color, error
    조리 중, 조리 완료, 조리 실패 타이밍이 되면 슬라이더 색상을 변화시키는 기능 추가.
    이제 완성을 시켜도 슬라이더 지속. 완성된 패티를 수거하지 않으면 타게 된다.

--- Assets/Scripts/BakeController.ts
+++ Assets/Scripts/BakeController.ts
@@ -1,79 +1,92 @@
 import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
-import { WaitForSeconds, Time, GameObject, Sprite, Debug } from 'UnityEngine';
+import { WaitForSeconds, Time, GameObject, Sprite, Debug, Color } from 'UnityEngine';
 import { Image, Button, Slider } from "UnityEngine.UI";
 
 export default class BakeController extends ZepetoScriptBehaviour {
-    public prepping: Button;    // Putting ingredients in the kitchen
-    public bakeSlider: Slider;
-    //public pattyImage: Image;
+    public bakingButton: Button;    // Putting ingredients in the kitchen
     public bakedButton: Button;
+    public bakeSlider: Slider;
+    public bakeSliderFill: Image;
+    public defaultColor: Color;
+    public bakedColor: Color;
+    public failedColor: Color;
     public bakedPattySprite: Sprite;
     public burntPattySprite: Sprite;
     public bakeTime: number;
     public burnTime: number; 
     private currentTime: number = 0;
+    private isFliped: bool;
     private isBaking: bool;
 
     Start() {
         Debug.Log(this.bakedButton.image.sprite);
-        Debug.Log(this.prepping.image.sprite);
+        Debug.Log(this.bakingButton.image.sprite);
         Debug.Log(this.bakedPattySprite);
         Debug.Log(this.burntPattySprite);
         this.bakedButton.interactable = false;
         this.bakedButton.gameObject.SetActive(false);
         this.bakeSlider.gameObject.SetActive(false);
-        this.prepping.onClick.AddListener(() => {this.StartBaking();});
+        this.bakingButton.onClick.AddListener(() => {this.StartBaking();});
     }
 
-    // 조리 시작
+    // Start Baking.
     private StartBaking() {
-        // 재료 버튼 비활성화
-        this.prepping.gameObject.SetActive(false);
-        // 불판 위에 위치한 버튼 이미지를 해당 음식 이미지로 변경
-        this.bakedButton.image.sprite = this.prepping.image.sprite;
-        // 불판 위 버튼 활성화
+
+        // Disable baking Button
+        this.bakingButton.gameObject.SetActive(false);
+        // Change grill button's sprite to baking button's sprite
+        this.bakedButton.image.sprite = this.bakingButton.image.sprite;
+        // Activate grill button
         this.bakedButton.gameObject.SetActive(true);
-        // 불판 위 타이머 슬라이더 활성화
+        // Enable timer slider
         this.bakeSlider.gameObject.SetActive(true);
     
         this.bakedButton.onClick.RemoveAllListeners();
-        //this.bakedButton.onClick.AddListener(this.FlipPatty);
         this.isBaking = true;
         this.StartCoroutine(this.DoBaking());
     }
 
-    // 베이킹 코루틴
+    // Baking Coroutine
     *DoBaking() {
         this.currentTime = 0;
+        this.isFliped = false;
         let flipCount = 0;
-        while (flipCount <2) {
+        this.bakeSliderFill.color = this.defaultColor;
+        while (this.isBaking) {
             this.currentTime += Time.deltaTime;
             this.bakeSlider.value = this.currentTime / this.burnTime;
 
-            if (this.currentTime >= this.bakeTime && this.isBaking) {
-                this.bakedButton.image.sprite = this.bakedPattySprite;
-                this.isBaking = false;
-                this.bakedButton.interactable = true;
-                this.bakedButton.onClick.RemoveAllListeners();
-                this.bakedButton.onClick.AddListener(() => { this.FlipPatty(); });
-                flipCount++;
+            if (this.currentTime >= this.bakeTime) {
+                if (flipCount > 0 && this.isFliped) {
+                    // baking done.
+                    this.bakedButton.interactable = true;
+                    this.bakedButton.onClick.RemoveAllListeners();
+                    this.bakedButton.onClick.AddListener(() => { this.ClearGrill(); });
+                    this.bakeSliderFill.color = this.bakedColor;
+                } 
+                else if (flipCount == 0 && !this.isFliped) {
+                    this.bakedButton.interactable = true;
+                    this.bakedButton.onClick.RemoveAllListeners();
+                    this.bakedButton.onClick.AddListener(() => { this.FlipPatty(); });
+                    this.bakeSliderFill.color = this.bakedColor;
+                    flipCount++;
+                }
             }
             if (this.currentTime >= this.burnTime) {
                 this.bakedButton.image.sprite = this.burntPattySprite;
+                this.bakeSliderFill.color = this.failedColor;
                 this.StopBaking();
-                break;
             }
             yield new WaitForSeconds(Time.deltaTime);
         }
-        // baking done.
-        this.StopBaking();
     }
 
     private FlipPatty() {
+        this.isFliped = true;
+        this.bakeSliderFill.color = this.defaultColor;
         this.currentTime = 0;
-        this.isBaking = true;
         this.bakedButton.interactable = false;
-        this.bakedButton.image.sprite = this.prepping.image.sprite;
+        this.bakedButton.image.sprite = this.bakedPattySprite;
     }
 
     private StopBaking() {
@@ -83,8 +96,9 @@ export default class BakeController extends ZepetoScriptBehaviour {
     }
 
     private ClearGrill() {
-        this.prepping.gameObject.SetActive(true);
+        this.bakingButton.gameObject.SetActive(true);
         this.isBaking = false;
+        this.isFliped = false;
         this.bakedButton.interactable = false;
         this.bakedButton.onClick.RemoveAllListeners();
         this.bakedButton.gameObject.SetActive(false);
